afe839a04c38cba765246f81478ce26a3c25367f
kwarn <karlwarner.dev@gmail.com>
2024-07-02 12:22:51 +0100

    add pub form field disabled until choosing a place

--- src/components/AddPub/AddPubForm.tsx
+++ src/components/AddPub/AddPubForm.tsx
@@ -23,10 +23,10 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
   const [formState, setFormState] = useState({
     name: "",
     address: "",
-    isCueDeposit: "Don't Know",
-    isJumpingAllowed: "Don't Know",
-    isPoundOnTable: "Don't Know",
-    isReservationAllowed: "Don't Know",
+    isCueDeposit: "",
+    isJumpingAllowed: "",
+    isPoundOnTable: "",
+    isReservationAllowed: "",
   });
 
   const [noSelectedPubError, setNoSelectedPubError] = useState<boolean>(false);
@@ -73,10 +73,10 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
         lng: place?.lng ?? 0,
       },
       rules: {
-        isCueDeposit: formState.isCueDeposit,
-        isJumpingAllowed: formState.isJumpingAllowed,
-        isPoundOnTable: formState.isPoundOnTable,
-        isReservationAllowed: formState.isReservationAllowed,
+        isCueDeposit: formState.isCueDeposit ?? "Don't Know",
+        isJumpingAllowed: formState.isJumpingAllowed ?? "Don't Know",
+        isPoundOnTable: formState.isPoundOnTable ?? "Don't Know",
+        isReservationAllowed: formState.isReservationAllowed ?? "Don't Know",
       },
     });
   };
@@ -132,7 +132,8 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
       {place && isNotPub && (
         <Alert $bgColor="#f3cca8">
           <p>
-            Are you sure this is a pub? Google thinks {"it's"} a {place?.type}.
+            Are you sure this is a pub?
+            <br /> Google thinks {"it's"} a {place?.type}.
             <br /> You can still add this venue but it will have to be manually
             reviewed before others can see it.
           </p>
@@ -145,24 +146,24 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
           <Button
             type="button"
             onClick={() => handleButtonToggle("isCueDeposit", "Yes")}
-            selected={formState.isCueDeposit === "Yes"}
-            disabled={loading}
+            $selected={formState.isCueDeposit === "Yes"}
+            disabled={loading || !place}
           >
             Yes
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isCueDeposit", "No")}
-            selected={formState.isCueDeposit === "No"}
-            disabled={loading}
+            $selected={formState.isCueDeposit === "No"}
+            disabled={loading || !place}
           >
             No
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isCueDeposit", "Don't Know")}
-            selected={formState.isCueDeposit === "Don't Know"}
-            disabled={loading}
+            $selected={formState.isCueDeposit === "Don't Know"}
+            disabled={loading || !place}
           >
             {"Don't Know"}
           </Button>
@@ -175,24 +176,24 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
           <Button
             type="button"
             onClick={() => handleButtonToggle("isJumpingAllowed", "Yes")}
-            selected={formState.isJumpingAllowed === "Yes"}
-            disabled={loading}
+            $selected={formState.isJumpingAllowed === "Yes"}
+            disabled={loading || !place}
           >
             Yes
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isJumpingAllowed", "No")}
-            selected={formState.isJumpingAllowed === "No"}
-            disabled={loading}
+            $selected={formState.isJumpingAllowed === "No"}
+            disabled={loading || !place}
           >
             No
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isJumpingAllowed", "Don't Know")}
-            selected={formState.isJumpingAllowed === "Don't Know"}
-            disabled={loading}
+            $selected={formState.isJumpingAllowed === "Don't Know"}
+            disabled={loading || !place}
           >
             {"Don't Know"}
           </Button>
@@ -205,24 +206,24 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
           <Button
             type="button"
             onClick={() => handleButtonToggle("isPoundOnTable", "Yes")}
-            selected={formState.isPoundOnTable === "Yes"}
-            disabled={loading}
+            $selected={formState.isPoundOnTable === "Yes"}
+            disabled={loading || !place}
           >
             Yes
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isPoundOnTable", "No")}
-            selected={formState.isPoundOnTable === "No"}
-            disabled={loading}
+            $selected={formState.isPoundOnTable === "No"}
+            disabled={loading || !place}
           >
             No
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isPoundOnTable", "Don't Know")}
-            selected={formState.isPoundOnTable === "Don't Know"}
-            disabled={loading}
+            $selected={formState.isPoundOnTable === "Don't Know"}
+            disabled={loading || !place}
           >
             {"Don't Know"}
           </Button>
@@ -235,16 +236,16 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
           <Button
             type="button"
             onClick={() => handleButtonToggle("isReservationAllowed", "Yes")}
-            selected={formState.isReservationAllowed === "Yes"}
-            disabled={loading}
+            $selected={formState.isReservationAllowed === "Yes"}
+            disabled={loading || !place}
           >
             Yes
           </Button>
           <Button
             type="button"
             onClick={() => handleButtonToggle("isReservationAllowed", "No")}
-            selected={formState.isReservationAllowed === "No"}
-            disabled={loading}
+            $selected={formState.isReservationAllowed === "No"}
+            disabled={loading || !place}
           >
             No
           </Button>
@@ -253,15 +254,15 @@ const AddPubForm: React.FC<AddPubFormProps> = ({
             onClick={() =>
               handleButtonToggle("isReservationAllowed", "Don't Know")
             }
-            selected={formState.isReservationAllowed === "Don't Know"}
-            disabled={loading}
+            $selected={formState.isReservationAllowed === "Don't Know"}
+            disabled={loading || !place}
           >
             {"Don't Know"}
           </Button>
         </ButtonGroup>
       </FormGroup>
 
-      <SubmitButton type="submit" disabled={loading}>
+      <SubmitButton type="submit" disabled={loading || !place}>
         {loading ? <Spinner size="sm" /> : "Add Pub"}
       </SubmitButton>
     </Form>
@@ -329,11 +330,11 @@ const ButtonGroup = styled.div`
   justify-content: space-between;
 `;
 
-const Button = styled.button<{ selected: boolean }>`
+const Button = styled.button<{ $selected: boolean }>`
   width: 100%;
   padding: 10px 15px;
-  background-color: ${(props) => (props.selected ? "#25913e" : "#EEE")};
-  color: ${(props) => (props.selected ? "#FFF" : "#333")};
+  background-color: ${(props) => (props.$selected ? "#25913e" : "#EEE")};
+  color: ${(props) => (props.$selected ? "#FFF" : "#333")};
   border: 1px solid #ccc;
   border-radius: 4px;
   font-size: 16px;
@@ -350,7 +351,7 @@ const Button = styled.button<{ selected: boolean }>`
   }
 
   &:hover:not(:disabled) {
-    background-color: ${(props) => (props.selected ? "#25913e" : "#DDD")};
+    background-color: ${(props) => (props.$selected ? "#25913e" : "#DDD")};
   }
 `;
 
